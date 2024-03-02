fa90837506882df6098b0056715ecf3e3b376646
wsrn829 <wsrn829@gmail.com>
2024-03-02 18:57:59 -0500

    Update About and rebuild

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
   return (
     <>
     <motion.div varients={textVariant()}>
-      <p className={`${styles.sectionSubText}`}>From Philosophy to Technology</p>
+      <p className={`${styles.sectionSubText} mt-3`}>From Philosophy to Technology</p>
       <h2 className={`${styles.sectionHeadText}`}>My Journey.</h2>
     </motion.div>
 
@@ -18,27 +18,19 @@ const About = () => {
       variants={fadeIn("", "", 0.1, 0.8)}
       className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
     >
-    “The limits of your language are the limits of your world,”
-    a profound statement by the Austrian philosopher Ludwig Wittgenstein,
-    has a special place in my heart. This is a sentence I first encountered in the preface
-    of the very first dictionary my dad bought for me when I was just a child. This simple
-    yet powerful message has stayed with me throughout my life, reminding me of the endless
-    possibilities that language and knowledge can unlock.
+    My passion for software engineering began at Oxford, where one of my PhD mentors had spent decades working on Digital Cultural Heritage. This led to my shift in interests from Social Science to Computer Science, and I was fascinated by the role software engineering played in traditionally non-technical fields like humanities, museums, and libraries.
     <br />
     <br />
-    My passion for software engineering began at Oxford, where one of my PhD mentors, 
-    now affiliated with the Department of Engineering Science, had spent decades working on 
-    Digital Cultural Heritage, which led to my shift in interests from Oriental Studies to 
-    Computer Science. *Fun fact: I defended my D.Phil dissertation in the Department 
-    of Computer Science building at Oxford, subtly foreshadowing my future career shift.*
-    <br />
-    <br />
-    As a dedicated Full-Stack Engineer with over 1000 hours of training in Agile environments, 
-    I bring hands-on experience spanning the entire software development lifecycle, from planning to implementation. 
+    As a dedicated Full-Stack Engineer with over 1000 hours of hands-on training in Agile environments, 
+    I bring expertise spanning the entire software development lifecycle. 
     Proficient in CI/CD processes, I specialize in ensuring smooth software deployments. Thriving in cross-functional teams, 
     I foster collaborative problem-solving to guarantee the timely delivery of high-quality software solutions. 
     Leveraging my background in social science research, I instill a user-centric approach, emphasizing the broader impact of development decisions. 
-    Let's work together to bring your ideas to life!
+    <br />
+    <br />
+    I am proficient in building responsive user interfaces with modern JavaScript frameworks like React and secure server-side architectures with Python and Django. 
+    My expertise encompasses working with both SQL and NoSQL databases and implementing CI/CD for projects. 
+    As a growth-minded developer, I am driven by an insatiable desire for knowledge and an unwavering commitment to excellence. Let's work together to bring your ideas to life!
     </motion.div>
     </>
   );
